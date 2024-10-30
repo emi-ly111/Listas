629bc74d9def43c39dafe90d09c3ff772d17bbae
emi-ly111 <emilyhgomez123@gmail.com>
2024-10-29 21:45:29 -0300

    Criando o arquivo do primeiro exercício e tentando entender como fazê-lo

---
+++ src/ex01.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+typedef struct Nodo {
+    int dado; //dado q vai ser armazenado
+    struct Nodo* proximo; //ponteiro q vai apontar pro proximo
+} Nodo; //????????seila
+
+/*
+10 -> 20 -> 30 -> NULL 
+cada numero possui um ponteiro q aponta para o proximo numero da lista, NULL é o final dela
+*/
+
+int main() {
+    /*criar um programa q inicialize uma lista encadeada e realize uma busca para verificar se um item x ta na lista.
+    como vou fazer isso eu nao faço a minimia ideia :D*/
+    return 0;
+}
